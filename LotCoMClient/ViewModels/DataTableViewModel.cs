e217852a8612f9ec07e9485bb62a22db1090c1b3
Mason Ritchason <masonritchason@gmail.com>
2025-03-27 12:55:27 -0400

    feat: implement ViewModel and Code-behind for DataTablePage view
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

---
+++ LotCoMClient/ViewModels/DataTableViewModel.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace LotCoMClient.ViewModels;
+
+/// <summary>
+/// ViewModel (ViewModel Layer) controlling the logic of the DataTablePage View class.
+/// Interacts with the Model Layer to invoke business logic and retrieve data.
+/// </summary>
+public partial class DataTableViewModel : ObservableObject {
+
+}
---
+++ LotCoMClient/Views/DataTablePage.xaml.cs
@@ -0,0 +1,23 @@
+namespace LotCoMClient.Views;
+
+/// <summary>
+/// Code-behind (View Layer) for the DataTablePage View.
+/// </summary>
+public partial class DataTablePage : ContentPage {
+    /// <summary>
+    /// ViewModel object controlling the logic of this Page.
+    /// </summary>
+	private readonly ViewModels.DataTableViewModel _viewModel;
+
+    /// <summary>
+    /// Creates a new DataTablePage.
+    /// </summary>
+    public DataTablePage() {
+		// instantiate the ViewModel
+        _viewModel = new ViewModels.DataTableViewModel();
+        BindingContext = _viewModel;
+
+        // create the page from XAML
+		InitializeComponent();
+    }
+}

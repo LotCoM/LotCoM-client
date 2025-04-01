1d1fe2c00da87e0c28c7ffa31f78b65c819b1a79
Mason Ritchason <masonritchason@gmail.com>
2025-04-01 09:58:42 -0400

    refactor(DataTableSelectorViewModel): utilize DataTable.RecordType to build data path
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMClient/ViewModels/DataTableSelectorViewModel.cs
+++ LotCoMClient/ViewModels/DataTableSelectorViewModel.cs
@@ -55,20 +55,21 @@ public partial class DataTableSelectorViewModel : DataTableViewModel {
     /// <param name="PageProcessPicker">The Process Picker control.</param>
     /// <returns></returns>
     public async Task UpdatePageProcess(Picker PageProcessPicker) {
-        // elicit the record type of this page using the current records in Data
-        string RecordType;
-        if (Data[0].GetType().Equals(typeof(PrintRecord))) {
+        // elicit the record type of this page using the current DataTable and set the path accordingly
+        Type RecordType = Table.RecordType;
+        string Path = "\\\\144.133.122.1\\Lot Control Management\\Database\\data_tables";
+        if (RecordType.Equals(typeof(PrintRecord))) {
             // page is displaying printing data
-            RecordType = "prints";
+            Path = $"{Path}\\prints";
         } else {
             // page is displaying scanning data
-            RecordType = "scans";
+            Path = $"{Path}\\scans";
         }
         // get the Process currently selected in the ProcessPicker control
         SelectedProcessIndex = PageProcessPicker.SelectedIndex;
         Process SelectedProcess = (Process)PageProcessPicker.ItemsSource[SelectedProcessIndex]!;
         // update the Page's DataTable to consume data from the newly selected Process Database Table
-        string Path = $"\\\\144.133.122.1\\Lot Control Management\\Database\\data_tables\\{RecordType}\\{SelectedProcess.FullName}.txt";
+        Path = $"{Path}\\{SelectedProcess.FullName}.txt";
         Table = new DataTable(Path);
         // update the Page's Data
         Data = await Table.GetRecordsAsync();

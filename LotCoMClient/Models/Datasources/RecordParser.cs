f19264206f14bbe2d0f2dc204dafcb5f6dc5f004
Mason Ritchason <masonritchason@gmail.com>
2025-04-15 12:13:30 -0400

    refactor(RecordParser): fully asynchronize and optimize parsing
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMClient/Models/Datasources/RecordParser.cs
+++ LotCoMClient/Models/Datasources/RecordParser.cs
@@ -11,6 +11,123 @@ namespace LotCoMClient.Models.Datasources;
 /// </remarks>
 public static class RecordParser {
     /// <summary>
+    /// Asynchronously checks if the first element of a split CSV Line is an IP address.
+    /// </summary>
+    /// <param name="SplitCSVLine">A CSV Line that has already been split by commas.</param>
+    /// <returns>Null if the first element is not an IP address; the IP address if it is.</returns>
+    private static async Task<string?> ParseIPAddressAsync(List<string> SplitCSVLine) {
+        return await Task.Run(() => {
+            // peek the first element and test it as an IP Address using a Regex pattern
+            string AddressPattern = @"^\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?$";
+            Regex AddressRegex = new Regex(AddressPattern);
+            if (AddressRegex.IsMatch(SplitCSVLine[0])) {
+                // return the first field (confirmed as an IP Address)
+                return SplitCSVLine[0];
+            } else {
+                return null;
+            }
+        });
+    }
+
+    /// <summary>
+    /// Asynchronously attempts to parse a valid Process object from the first element of a split CSV Line.
+    /// </summary>
+    /// <remarks>
+    /// Throws RecordParseException if the Process in the CSV Line was not parsable into a defined Process.
+    /// </remarks>
+    /// <param name="SplitCSVLine">A CSV Line that has already been split by commas.</param>
+    /// <returns>A Process object, as parsed from the CSV Line.</returns>
+    /// <exception cref="RecordParseException"></exception>
+    private static async Task<Process> ParseRecordProcessAsync(List<string> SplitCSVLine) {
+        // confirm that the Process is a valid process
+        try {
+            return await ProcessData.GetIndividualProcessDataAsync(SplitCSVLine[0]);
+        } catch {
+            throw new RecordParseException($"The Process {SplitCSVLine[0]} is not defined.");
+        }
+    }
+
+    /// <summary>
+    /// Asynchronously attempts to parse a valid Part object from the first element of a split CSV Line.
+    /// Uses the Parts associated with ParsedProcess to validate the parse.
+    /// </summary>
+    /// <remarks>
+    /// Throws RecordParseException if the Part in the CSV Line was not parsable into a defined Part on ParsedProcess.
+    /// </remarks>
+    /// <param name="SplitCSVLine">A CSV Line that has already been split by commas.</param>
+    /// <param name="ParsedProcess">A Process Object.</param>
+    /// <returns>A Part object, as parsed from the CSV Line.</returns>
+    /// <exception cref="RecordParseException"></exception>
+    private static async Task<Part> ParseRecordPartAsync(List<string> SplitCSVLine, Process ParsedProcess) {
+        // confirm that the Part Number and Part Name belong to a valid part
+        try {
+            return await PartData.GetPartDataAsync(ParsedProcess.FullName, SplitCSVLine[1]);
+        } catch {
+            throw new RecordParseException($"The Part {SplitCSVLine[1]} {SplitCSVLine[2]} is not defined for Process {ParsedProcess.FullName}");
+        }
+    }
+
+    /// <summary>
+    /// Asynchronously attempts to parse each of the variably-required data fields for ParsedProcess from a split CSV Line.
+    /// </summary>
+    /// <param name="SplitCSVLine">A CSV Line that has already been split by commas.</param>
+    /// <param name="ParsedProcess">A Process Object.</param>
+    /// <returns></returns>
+    private static async Task<VariableFieldSet> ParseVariableFieldSetAsync(List<string> SplitCSVLine, Process ParsedProcess) {
+        return await Task.Run(() => {
+            // parse the required variable data fields
+            VariableFieldSet VariableSet = new VariableFieldSet();
+            List<string> Requirements = ParsedProcess.RequiredFields;
+            // references next parsable index after the static Quantity, position 4 (index 3); 
+            // increments when a property is found to be required and is assigned a parsable index
+            int _parsingIndex = 0;
+            // attempt to parse a JBK number
+            if (Requirements.Contains("JBKNumber")) {
+                // assign the value of the current parsing index to the JBK Number property
+                VariableSet.JBKNumber = SplitCSVLine[4 + _parsingIndex];
+                // increment to the next parsable index
+                _parsingIndex += 1;
+            }
+            // attempt to parse a Lot number
+            if (Requirements.Contains("LotNumber")) {
+                // assign the value of the current parsing index to the Lot Number property
+                VariableSet.LotNumber = SplitCSVLine[4 + _parsingIndex];
+                // increment to the next parsable index
+                _parsingIndex += 1;
+            }
+            // attempt to parse a Deburr JBK number
+            if (Requirements.Contains("DeburrJBKNumber")) {
+                // assign the value of the current parsing index to the Deburr JBK Number property
+                VariableSet.DeburrJBKNumber = SplitCSVLine[4 + _parsingIndex];
+                // increment to the next parsable index
+                _parsingIndex += 1;
+            }
+            // attempt to parse a Die number
+            if (Requirements.Contains("DieNumber")) {
+                // assign the value of the current parsing index to the Die Number property
+                VariableSet.DieNumber = SplitCSVLine[4 + _parsingIndex];
+                // increment to the next parsable index
+                _parsingIndex += 1;
+            }
+            // attempt to parse a Model number
+            if (Requirements.Contains("ModelNumber")) {
+                // assign the value of the current parsing index to the Model Number property
+                VariableSet.ModelNumber = SplitCSVLine[4 + _parsingIndex];
+                // increment to the next parsable index
+                _parsingIndex += 1;
+            }
+            // attempt to parse a Heat number
+            if (Requirements.Contains("HeatNumber")) {
+                // assign the value of the current parsing index to the Heat Number property
+                VariableSet.HeatNumber = SplitCSVLine[4 + _parsingIndex];
+                // increment to the next parsable index
+                _parsingIndex += 1;
+            }
+            return VariableSet;
+        });
+    }
+
+    /// <summary>
     /// Attempts to parse a DataRecord object from a CSV Line.
     /// </summary>
     /// <remarks>
@@ -20,98 +137,38 @@ public static class RecordParser {
     /// <param name="CSVLine"></param>
     /// <returns>A DataRecord object.</returns>
     /// <exception cref="RecordParseException"></exception>
-    public static DataRecord ParseFromCSV(string CSVLine) {
+    public static async Task<DataRecord> ParseFromCSVAsync(string CSVLine) {
         // split the CSV Line by commas
         List<string> SplitLine = CSVLine.Split(",").ToList();
         // confirm that the split list contains 8 universal fields and AT LEAST 1 variable field
         if (SplitLine.Count < 7) {
             throw new RecordParseException();
         }
-        // peek the first element and test it as an IP Address using a Regex pattern
-        string? ScanAddress = null;
-        string AddressPattern = @"^\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?$";
-        Regex AddressRegex = new Regex(AddressPattern);
-        if (AddressRegex.IsMatch(SplitLine[0])) {
-            // set the ScanAddress property using this field and pop it off of the front
-            ScanAddress = SplitLine[0];
+        // composite asynchronous tasks to parse universal data fields quickly
+        string? ScanAddress;
+        ScanAddress = await ParseIPAddressAsync(SplitLine);
+        // remove the IP address from the SplitLine list (if parsed)
+        if (ScanAddress != null) {
             SplitLine.RemoveAt(0);
         }
-        // prepare remaining universal DataRecord properties
         Process RecordProcess;
         Part RecordPart;
+        try {
+            RecordProcess = await ParseRecordProcessAsync(SplitLine);
+            RecordPart = await ParseRecordPartAsync(SplitLine, RecordProcess);
+        } catch {
+            throw new RecordParseException();
+        }
         string Quantity = SplitLine[3];
-        string JBKNumber = "";
-        string LotNumber = "";
-        string DeburrJBKNumber = "";
-        string DieNumber = "";
-        string ModelNumber = "";
-        string HeatNumber = "";
         string RecordDate = SplitLine[^3].Split("-")[0];
         string RecordTime = SplitLine[^3].Split("-")[1];
         string RecordShift = SplitLine[^2];
         string OperatorID = SplitLine[^1];
-        // confirm that the Process is a valid process
-        try {
-            RecordProcess = ProcessData.GetIndividualProcessData(SplitLine[0]);
-        } catch {
-            throw new RecordParseException($"The Process {SplitLine[0]} is not defined.");
-        }
-        // confirm that the Part Number and Part Name belong to a valid part
-        try {
-            RecordPart = PartData.GetPartData(RecordProcess.FullName, SplitLine[1]);
-        } catch {
-            throw new RecordParseException($"The Part {SplitLine[1]} {SplitLine[2]} is not defined for Process {RecordProcess.FullName}");
-        }
-        // parse the required variable data fields
-        List<string> Requirements = RecordProcess.RequiredFields;
-        // references next parsable index after the static Quantity, position 4 (index 3); 
-        // increments when a property is found to be required and is assigned a parsable index
-        int _parsingIndex = 0;
-        // attempt to parse a JBK number
-        if (Requirements.Contains("JBKNumber")) {
-            // assign the value of the current parsing index to the JBK Number property
-            JBKNumber = SplitLine[4 + _parsingIndex];
-            // increment to the next parsable index
-            _parsingIndex += 1;
-        }
-        // attempt to parse a Lot number
-        if (Requirements.Contains("LotNumber")) {
-            // assign the value of the current parsing index to the Lot Number property
-            LotNumber = SplitLine[4 + _parsingIndex];
-            // increment to the next parsable index
-            _parsingIndex += 1;
-        }
-        // attempt to parse a Deburr JBK number
-        if (Requirements.Contains("DeburrJBKNumber")) {
-            // assign the value of the current parsing index to the Deburr JBK Number property
-            DeburrJBKNumber = SplitLine[4 + _parsingIndex];
-            // increment to the next parsable index
-            _parsingIndex += 1;
-        }
-        // attempt to parse a Die number
-        if (Requirements.Contains("DieNumber")) {
-            // assign the value of the current parsing index to the Die Number property
-            DieNumber = SplitLine[4 + _parsingIndex];
-            // increment to the next parsable index
-            _parsingIndex += 1;
-        }
-        // attempt to parse a Model number
-        if (Requirements.Contains("ModelNumber")) {
-            // assign the value of the current parsing index to the Model Number property
-            ModelNumber = SplitLine[4 + _parsingIndex];
-            // increment to the next parsable index
-            _parsingIndex += 1;
-        }
-        // attempt to parse a Heat number
-        if (Requirements.Contains("HeatNumber")) {
-            // assign the value of the current parsing index to the Heat Number property
-            HeatNumber = SplitLine[4 + _parsingIndex];
-            // increment to the next parsable index
-            _parsingIndex += 1;
-        }
+        // attempt to parse any variably-required fields
+        VariableFieldSet VariableFields = await ParseVariableFieldSetAsync(SplitLine, RecordProcess);
         // attempt to create a DataRecord from the parsed data
         try {
-            return new DataRecord(RecordProcess, RecordPart, Quantity, JBKNumber, LotNumber, DeburrJBKNumber, DieNumber, ModelNumber, HeatNumber, RecordDate, RecordTime, RecordShift, OperatorID, ScanAddress);
+            return new DataRecord(RecordProcess, RecordPart, Quantity, VariableFields.JBKNumber, VariableFields.LotNumber, VariableFields.DeburrJBKNumber, VariableFields.DieNumber, VariableFields.ModelNumber, VariableFields.HeatNumber, RecordDate, RecordTime, RecordShift, OperatorID, ScanAddress);
         // there was a problem constructing a DataRecord from the parsed data
         } catch {
             throw new RecordParseException();

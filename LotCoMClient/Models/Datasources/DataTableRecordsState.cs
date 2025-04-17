e172b4e1d689bd3b733e3ba2dc5da88d3f902f01
Mason Ritchason <masonritchason@gmail.com>
2025-04-17 15:42:21 -0400

    refactor(DataTableRecordsState): change property types to match DataTable
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMClient/Models/Datasources/DataTableRecordsState.cs
+++ LotCoMClient/Models/Datasources/DataTableRecordsState.cs
@@ -1,5 +1,3 @@
-using LotCoMClient.Models.Services;
-
 namespace LotCoMClient.Models.Datasources;
 
 public class DataTableRecordsState 
@@ -7,13 +5,13 @@ public class DataTableRecordsState
     /// <summary>
     /// List of DataRecords that is unmodified since the last ReadAsync() call.
     /// </summary>
-    public NotifyTaskCompletion<List<DataRecord>>? LastRead;
+    public List<DataRecord>? LastRead;
 
     /// <summary>
     /// An unprotected List of DataRecords that contains the current state of the DataRecords List.
     /// Can be Sorted and Filtered using the corresponding DataTable methods.
     /// </summary>
-    public NotifyTaskCompletion<List<DataRecord>>? Current;
+    public List<DataRecord>? Current;
 
     /// <summary>
     /// Creates a DataTableRecordsState to track a DataTable object's DataRecord states.
@@ -21,23 +19,5 @@ public class DataTableRecordsState
     public DataTableRecordsState() 
     {
     }
-
-    /// <summary>
-    /// Updates the LastRead DataRecord State.
-    /// </summary>
-    /// <param name="LastRead"></param>
-    public void SetLastRead(NotifyTaskCompletion<List<DataRecord>> LastRead) 
-    {
-        this.LastRead = LastRead;
-    }
-
-    /// <summary>
-    /// Updates the Current DataRecord State.
-    /// </summary>
-    /// <param name="LastRead"></param>
-    public void SetCurrent(NotifyTaskCompletion<List<DataRecord>> Current) 
-    {
-        this.Current = Current;
-    }
-
+    
 }

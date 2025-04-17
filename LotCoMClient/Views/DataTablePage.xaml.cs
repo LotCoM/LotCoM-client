c81c95948920985dae19353308b0bc316748a15b
Mason Ritchason <masonritchason@gmail.com>
2025-04-17 16:16:14 -0400

    refactor(DataTablePage): asynchronize Search and Sort methods
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMClient/Views/DataTablePage.xaml.cs
+++ LotCoMClient/Views/DataTablePage.xaml.cs
@@ -5,7 +5,8 @@ namespace LotCoMClient.Views;
 /// <summary>
 /// Code-behind (View Layer) for the DataTablePage View.
 /// </summary>
-public partial class DataTablePage : ContentPage {
+public partial class DataTablePage : ContentPage 
+{
     /// <summary>
     /// ViewModel object controlling the logic of this Page.
     /// </summary>
@@ -15,25 +16,34 @@ public partial class DataTablePage : ContentPage {
     /// Asynchronously evaluates the state of the Data property and sets the BodyTableHeader property accordingly.
     /// If Data is loaded, shows the Record count. Else, shows "Loading Records...".
     /// </summary>
-    private async Task ConfigureBodyTableHeader() {
-        await Task.Run(() => {
+    private async Task ConfigureBodyTableHeader() 
+    {
+        await Task.Run(() => 
+        {
             // get the count of Data entries
             int DataCount;
             // do not do any processing if Data is null (no-Process instantiation)
-            if (_viewModel.Data == null) {
+            if (_viewModel.Data == null) 
+            {
                 return;
             }
             // the Data property is set and is either loading or completed
-            if (_viewModel.Data!.IsCompleted && _viewModel.Data.Result != null) {
+            if (_viewModel.Data!.IsCompleted && _viewModel.Data.Result != null) 
+            {
                 DataCount = _viewModel.Data.Result.Count;
                 // update the BodyTableHeader to show the item count
-                if (DataCount > 1) {
+                if (DataCount > 1) 
+                {
                     _viewModel.BodyTableHeader = $"Showing {DataCount} records";
-                } else {
+                } 
+                else 
+                {
                     _viewModel.BodyTableHeader = $"Showing {DataCount} records";
                 }
             // the data is not loaded yet; default BodyTableHeader property
-            } else {
+            } 
+            else 
+            {
                 _viewModel.BodyTableHeader = "Loading records...";
             }
         });
@@ -43,34 +53,44 @@ public partial class DataTablePage : ContentPage {
     /// Checks that the Table has DataRecords available. Updates the Data fields based on that Data.
     /// </summary>
     /// <returns></returns>
-    private async Task ConfigureDataFields() {
+    private async Task ConfigureDataFields() 
+    {
         // perform the config logic on a new CPU thread
-        await Task.Run(() => {
+        await Task.Run(() => 
+        {
             // confirm that the Data property has completed its async task
-            if (_viewModel.Data == null || _viewModel.Data.IsNotCompleted) {
+            if (_viewModel.Data == null || _viewModel.Data.IsNotCompleted) 
+            {
                 return;
             }
             List<string> Sortables = ["Part Number", "Part Name", "Quantity"];
             // add the variably-required DataRecord fields (only if Data is loaded)
             DataRecord SampleRecord;
-            if (_viewModel.Data.Result != null && _viewModel.Data.Result.Count > 0) {
+            if (_viewModel.Data.Result != null && _viewModel.Data.Result.Count > 0) 
+            {
                 SampleRecord = _viewModel.Data.Result[0];
-                if (SampleRecord.IncludesJBKNumber) {
+                if (SampleRecord.IncludesJBKNumber) 
+                {
                     Sortables.Add("JBK Number");
                 }
-                if (SampleRecord.IncludesLotNumber) {
+                if (SampleRecord.IncludesLotNumber) 
+                {
                     Sortables.Add("Lot Number");
                 }
-                if (SampleRecord.IncludesDeburrJBKNumber) {
+                if (SampleRecord.IncludesDeburrJBKNumber) 
+                {
                     Sortables.Add("Deburr JBK Number");
                 }
-                if (SampleRecord.IncludesDieNumber) {
+                if (SampleRecord.IncludesDieNumber) 
+                {
                     Sortables.Add("Die Number");
                 }
-                if (SampleRecord.IncludesModelNumber) {
+                if (SampleRecord.IncludesModelNumber) 
+                {
                     Sortables.Add("Model Number");
                 }
-                if (SampleRecord.IncludesHeatNumber) {
+                if (SampleRecord.IncludesHeatNumber) 
+                {
                     Sortables.Add("Heat Number");
                 }
             }
@@ -88,7 +108,8 @@ public partial class DataTablePage : ContentPage {
     /// <param name="PageTitle">A string to apply as the Page's Title.</param>
     /// <param name="RecordType">The subclass of DataRecord this Page is meant to display (PrintRecord || ScanRecord).</param>
     /// <param name="IsProcessAssigned">Indicates whether the Selector Page has been assigned a Process (True by default).</param>
-    public DataTablePage(string DataTablePath, string PageTitle, Type RecordType, bool IsProcessAssigned = true) {
+    public DataTablePage(string DataTablePath, string PageTitle, Type RecordType, bool IsProcessAssigned = true) 
+    {
 		// instantiate the ViewModel
         _viewModel = new ViewModels.DataTableViewModel(DataTablePath, PageTitle, RecordType, IsProcessAssigned);
         BindingContext = _viewModel;
@@ -102,10 +123,12 @@ public partial class DataTablePage : ContentPage {
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
-    public async void OnPageLeftFrameCollapseButtonClicked(object sender, EventArgs e) {
+    public async void OnPageLeftFrameCollapseButtonClicked(object sender, EventArgs e) 
+    {
         await Task.Delay(0);
         // the Panel needs to collapse
-        if (_viewModel.LeftFrameShown) {
+        if (_viewModel.LeftFrameShown) 
+        {
             // set the Left Panel properties in the ViewModel
             _viewModel.LeftFrameShown = false;
             _viewModel.LeftFrameHidden = true;
@@ -121,7 +144,9 @@ public partial class DataTablePage : ContentPage {
             //     await Task.Delay(1);
             // }
         // the Panel needs to raise
-        } else {
+        } 
+        else 
+        {
             // set the Left Panel properties in the ViewModel
             _viewModel.LeftFrameShown = true;
             _viewModel.LeftFrameHidden = false;
@@ -144,7 +169,8 @@ public partial class DataTablePage : ContentPage {
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
-    public async void OnPageDataTableListViewPropertyChanged(object sender, EventArgs e) {
+    public async void OnPageDataTableListViewPropertyChanged(object sender, EventArgs e) 
+    {
         // update BodyTableHeader property
         await ConfigureBodyTableHeader();
         await ConfigureDataFields();
@@ -155,12 +181,16 @@ public partial class DataTablePage : ContentPage {
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
-    public async void OnSortParameterSelectedIndexChanged(object sender, EventArgs e) {
-        // update ViewModel sorting indexes
-        _viewModel.SelectedSortingFieldIndex = ListViewSortingFieldPicker.SelectedIndex;
-        _viewModel.SelectedSortingOrderIndex = ListViewSortingOrderPicker.SelectedIndex;
-        // invoke the ViewModel sort method
-        await _viewModel.SortDataTable();
+    public async void OnSortParameterSelectedIndexChanged(object sender, EventArgs e) 
+    {
+        await Task.Run(() => 
+        {
+            // update ViewModel sorting indexes
+            _viewModel.SelectedSortingFieldIndex = ListViewSortingFieldPicker.SelectedIndex;
+            _viewModel.SelectedSortingOrderIndex = ListViewSortingOrderPicker.SelectedIndex;
+            // invoke the ViewModel sort method
+            _viewModel.SortDataTable();
+        });
     }
 
     /// <summary>
@@ -168,10 +198,14 @@ public partial class DataTablePage : ContentPage {
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
-    public async void OnListViewSearchButtonPressed(object sender, EventArgs e) {
-        // invoke the ViewModel local sort method using the current search term
-        _viewModel.SearchTerm = ListViewSearchingSearchBar.Text;
-        _viewModel.SelectedSearchingFieldIndex = ListViewSearchingFieldPicker.SelectedIndex;
-        await _viewModel.SearchDataTable();
+    public async void OnListViewSearchButtonPressed(object sender, EventArgs e) 
+    {
+        await Task.Run(() => 
+        {
+            // invoke the ViewModel local sort method using the current search term
+            _viewModel.SearchTerm = ListViewSearchingSearchBar.Text;
+            _viewModel.SelectedSearchingFieldIndex = ListViewSearchingFieldPicker.SelectedIndex;
+            _viewModel.SearchDataTable();
+        });
     }
 }

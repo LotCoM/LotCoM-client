96eed5ee2130d9341dfc97eada27384e1c689626
Mason Ritchason <masonritchason@gmail.com>
2025-04-24 14:39:16 -0400

    refactor(PageSet): implement more native Page and PageSet functionality
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMClient/Models/Datasources/Page.cs
+++ LotCoMClient/Models/Datasources/Page.cs
@@ -4,8 +4,13 @@ namespace LotCoMClient.Models.Datasources;
 /// Creates a set of Lines and/or DataRecords that can be displayed in a ListView.
 /// </summary>
 /// <param name="PageLength">The maximum number of Lines/DataRecords this Page can hold.</param>
-public class Page(int PageLength)
+public class Page(int PageLength, Type RecordType)
 {
+    /// <summary>
+    /// Sets the type of DataRecord that this Page can hold.
+    /// </summary>
+    private Type RecordType = RecordType;
+
     /// <summary>
     /// The maximum number of Lines/DataRecords this Page can hold.
     /// </summary>
@@ -25,4 +30,47 @@ public class Page(int PageLength)
     /// Contains a parsed list of DataRecord objects assigned to this Page.
     /// </summary>
     public List<DataRecord> DataRecords = [];
+
+    /// <summary>
+    /// Parses a DataRecord of the Page's RecordType from CSVLine.
+    /// </summary>
+    /// <param name="Parser">A RecordParser object.</param>
+    /// <param name="CSVLine">A CSV-formatted string.</param>
+    /// <returns></returns>
+    private async Task<DataRecord> ParseRecordAsync(RecordParser Parser, string CSVLine)
+    {
+        // attempt to parse the proper type of DataRecord from the CSV Line
+        DataRecord ParsedRecord;
+        if (RecordType == typeof(PrintRecord)) 
+        {
+            ParsedRecord = await Parser.ParsePrintRecordFromCSVAsync(CSVLine);
+        // parse a ScanRecord
+        } 
+        else 
+        {
+            ParsedRecord = await Parser.ParseScanRecordFromCSVAsync(CSVLine);
+        }
+        // return the parsed DataRecord
+        return ParsedRecord;
+    }
+
+    /// <summary>
+    /// Parses all of the Lines in Page.Lines into DataRecords of Page.RecordType.
+    /// Stores the DataRecords in Page.DataRecords.
+    /// </summary>
+    public async Task GenerateDataRecords() 
+    {
+        // create a new RecordParser instance
+        RecordParser Parser = new RecordParser();
+        // parse a DataRecord from every Line in Lines and save it in DataRecords
+        IEnumerable<Task<DataRecord>>? ParseTasks = Lines
+            .Select(x => ParseRecordAsync(Parser, x));
+        DataRecord[]? ParseResults = await Task.WhenAll(ParseTasks);
+        // confirm that the Parse was successful and add the Parsed DataRecords to DataRecords
+        if (ParseResults is null) 
+        {
+            DataRecords = [];
+        }
+        DataRecords = ParseResults!.ToList();
+    }
 }
--- LotCoMClient/Models/Datasources/PageSet.cs
+++ LotCoMClient/Models/Datasources/PageSet.cs
@@ -3,13 +3,35 @@ namespace LotCoMClient.Models.Datasources;
 /// <summary>
 /// Provides a control structure for a set of Page objects that can be used in the same context.
 /// </summary>
-/// <param name="MaxCount">(Optional) Constrains the amount of Pages that are allowed in the PageSet.</param>
-public class PageSet(int MaxCount = -1)
+public class PageSet
 {
     /// <summary>
     /// Sets a maximum number of Pages allowed in this PageSet.
     /// </summary>
-    private int MaxCount = MaxCount;
+    private int MaxCount = -1;
+
+    /// <summary>
+    /// Sets the maximum number of Lines allowed in each Page in this PageSet.
+    /// </summary>
+    private int PageLength = 25;
+
+    /// <summary>
+    /// Sets the type of DataRecord that the Pages in this PageSet can hold.
+    /// </summary>
+    private Type RecordType;
+
+    private List<Page> _pages = [];
+    /// <summary>
+    /// The Page objects included in this PageSet.
+    /// </summary>
+    public List<Page> Pages
+    {
+        get {return _pages;}
+        private set
+        {
+            _pages = value;
+        }
+    }
 
     /// <summary>
     /// The number of Page objects in this PageSet.
@@ -32,16 +54,87 @@ public class PageSet(int MaxCount = -1)
         }
     }
 
-    private List<Page> _pages = [];
     /// <summary>
-    /// The Page objects included in this PageSet.
+    /// Creates a List of Page objects from Lines.
     /// </summary>
-    public List<Page> Pages
+    /// <param name="Lines">A List of Lines to create Pages from.</param>
+    /// <returns></returns>
+    private List<Page> Paginate(List<string> Lines)
     {
-        get {return _pages;}
-        private set
+        // create pages of PageLength Lines, starting with the newest lines
+        List<Page> GeneratedPages = [];
+        int TakenLines = 0;
+        while (TakenLines < Lines.Count)
         {
-            _pages = value;
+            // take the first PageLength + TakenLines elements from Lines to create a Page
+            Page _page = new Page(PageLength, RecordType);
+            _page.Lines = Lines
+                .Skip(TakenLines)
+                .Take(PageLength)
+                .ToList();
+            // increment the amount of lines taken and add the Page to the Table
+            TakenLines += PageLength;
+            GeneratedPages.Add(_page);
         }
+        return GeneratedPages;
+    }
+
+    /// <summary>
+    /// Creates a new, empty PageSet.
+    /// </summary>
+    /// <param name="MaxCount">(Optional) Constrains the amount of Pages that are allowed in the PageSet.</param>
+    /// <param name="PageLength">
+    /// (Optional) Sets the maximum number of Lines that are allowed in each Page in the PageSet.
+    /// Default is 25 Lines per Page object.
+    /// </param>
+    public PageSet(Type RecordType, int MaxCount = -1, int PageLength = 25)
+    {
+        this.MaxCount = MaxCount;
+        this.PageLength = PageLength;
+        this.RecordType = RecordType;
+    }
+
+    /// <summary>
+    /// Creates a new PageSet and pre-populates it with Pages created from Lines.
+    /// </summary>
+    /// <param name="Lines">A List of strings to use as Lines for each Page in the PageSet.</param>
+    /// <param name="MaxCount">(Optional) Constrains the amount of Pages that are allowed in the PageSet.</param>
+    /// <param name="PageLength">
+    /// (Optional) Sets the maximum number of Lines that are allowed in each Page in the PageSet.
+    /// Default is 25 Lines per Page object.
+    /// </param>
+    public PageSet(List<string> Lines, Type RecordType, int MaxCount = -1, int PageLength = 25)
+    {
+        this.MaxCount = MaxCount;
+        this.PageLength = PageLength;
+        this.RecordType = RecordType;
+        // paginate Lines and set the Pages property
+        List<Page> GeneratedPages = Paginate(Lines);
+        if (MaxCount != -1)
+        {
+            Pages = GeneratedPages.Take(MaxCount).ToList();
+        }
+        else
+        {
+            Pages = GeneratedPages;
+        }
+    }
+
+    /// <summary>
+    /// Returns a Page with DataRecords.
+    /// </summary>
+    /// <param name="PageNumber">The index of the requested Page in PageSet.Pages. 0-oriented.</param>
+    /// <returns></returns>
+    public async Task<Page> GetPage(int PageNumber)
+    {
+        // confirm that a Page exists at the requested Page Number
+        if (Count - 1 < PageNumber) 
+        {
+            throw new IndexOutOfRangeException($"There is no Page at the requested index {PageNumber}.");
+        }
+        Page RequestedPage = Pages[PageNumber];
+        // populate the Page's DataRecords property
+        await RequestedPage.GenerateDataRecords();
+        return RequestedPage;
     }
 }

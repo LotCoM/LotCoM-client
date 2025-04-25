fd598f59cde870fb4bfdcb9fd91204159886a9d3
Mason Ritchason <masonritchason@gmail.com>
2025-04-25 15:57:27 -0400

    feat(DataTablePageOptions): implement set Defaults
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMClient/Models/Options/DataTablePageOptions.cs
+++ LotCoMClient/Models/Options/DataTablePageOptions.cs
@@ -18,76 +18,103 @@ public partial class DataTablePageOptions() : ObservableObject()
     }
 
     /// <summary>
+    /// Provides default values for most Options.
+    /// </summary>
+    public static class OptionDefaults
+    {
+        public const string Title = "";
+        public const bool IsBodyHeaderLabelShown = false;
+        public const string BodyHeaderText = "";
+        public const bool IsBodyNavigationPanelShown = false;
+        public const int PageNumber = 0;
+        public const int SelectedPageLengthIndex = 1;
+        public const int PageLength = 25;
+        public const string LeftPanelHeaderText = "";
+        public const string LeftPanelFooterText = "Click to Collapse";
+        public const bool IsLeftPanelShown = true;
+        public const bool IsLeftPanelHidden = false;
+        public const int LeftPanelWidth = (int)LeftPanelWidths.Open;
+        public const int SelectedSortingFieldIndex = -1;
+        public const int SelectedSortingOrderIndex = 1;
+        public const int SelectedSearchingFieldIndex = 0;
+        public const string SearchTerm = "";
+        public const Department? Department = null;
+        public const Process? Process = null;
+        public const int SelectedProcessIndex = -1;
+        public const bool IsProcessAssigned = false;
+    }
+
+    /// <summary>
     /// Controls the Page's Title which appears in the top-left corner.
     /// </summary>
     [ObservableProperty]
-    public partial string Title {get; set;} = "";
+    public partial string Title {get; set;} = OptionDefaults.Title;
 
     /// <summary>
     /// Controls the visibility of the Page's Body Header which is between the Page's Title and Body (ListView).
     /// </summary>
     [ObservableProperty]
-    public partial bool IsBodyHeaderLabelShown {get; set;} = false;
+    public partial bool IsBodyHeaderLabelShown {get; set;} = OptionDefaults.IsBodyHeaderLabelShown;
 
     /// <summary>
     /// Controls the text of the Page's Body Table Header.
     /// </summary>
     [ObservableProperty]
-    public partial string BodyHeaderText {get; set;} = "";
+    public partial string BodyHeaderText {get; set;} = OptionDefaults.BodyHeaderText;
 
     /// <summary>
     /// Controls the visibility of the Page's Navigation Panel which is between the Page's Title and Body (ListView).
     /// </summary>
     [ObservableProperty]
-    public partial bool IsBodyNavigationPanelShown {get; set;} = false;
+    public partial bool IsBodyNavigationPanelShown {get; set;} = OptionDefaults.IsBodyNavigationPanelShown;
 
     /// <summary>
     /// Controls the Number of the currently displayed Page of DataRecords, controlled by the Page's Navigation Panel.
     /// </summary>
     [ObservableProperty]
-    public partial int PageNumber {get; set;} = 0;
+    public partial int PageNumber {get; set;} = OptionDefaults.PageNumber;
 
     /// <summary>
     /// Controls the currently selected index of the Page's PageLength Picker.
     /// </summary>
     [ObservableProperty]
-    public partial int SelectedPageLengthIndex {get; set;} = 1;
+    public partial int SelectedPageLengthIndex {get; set;} = OptionDefaults.SelectedPageLengthIndex;
 
     /// <summary>
     /// Controls the number of DataRecords displayed by each Page of the DataTablePage's ListView, controlled by the Page's Navigation Panel.
     /// </summary>
     [ObservableProperty]
-    public partial int PageLength {get; set;} = 25;
+    public partial int PageLength {get; set;} = OptionDefaults.PageLength;
 
     /// <summary>
     /// Controls the text shown in the Page's Left Panel Header which appears at the top of the collapsable Left Panel.
     /// </summary>
     [ObservableProperty]
-    public partial string LeftPanelHeaderText {get; set;} = "";
+    public partial string LeftPanelHeaderText {get; set;} = OptionDefaults.LeftPanelHeaderText;
 
     /// <summary>
     /// Controls the text shown in the Page's Left Panel Footer which appears at the bottom of the collapsable Left Panel.
     /// </summary>
     [ObservableProperty]
-    public partial string LeftPanelFooterText {get; set;} = "Click to Collapse";
+    public partial string LeftPanelFooterText {get; set;} = OptionDefaults.LeftPanelFooterText;
 
     /// <summary>
     /// Controls the Page's Left Panel Shown state (boolean).
     /// </summary>
     [ObservableProperty]
-    public partial bool IsLeftPanelShown {get; set;} = true;
+    public partial bool IsLeftPanelShown {get; set;} = OptionDefaults.IsLeftPanelShown;
 
     /// <summary>
     /// Controls the Page's Left Panel Hidden state (boolean).
     /// </summary>
     [ObservableProperty]
-    public partial bool IsLeftPanelHidden {get; set;} = false;
+    public partial bool IsLeftPanelHidden {get; set;} = OptionDefaults.IsLeftPanelHidden;
 
     /// <summary>
     /// Controls the width of the Page's Left Panel.
     /// </summary>
     [ObservableProperty]
-    public partial int LeftPanelWidth {get; set;} = (int)LeftPanelWidths.Open;
+    public partial int LeftPanelWidth {get; set;} = OptionDefaults.LeftPanelWidth;
     
     /// <summary>
     /// Controls the data fields that are included in DataRecords for the Page's ListView.
@@ -124,31 +151,31 @@ public partial class DataTablePageOptions() : ObservableObject()
     /// Controls the currently selected index of the Page's SortingField Picker.
     /// </summary>
     [ObservableProperty]
-    public partial int SelectedSortingFieldIndex {get; set;} = -1;
+    public partial int SelectedSortingFieldIndex {get; set;} = OptionDefaults.SelectedSortingFieldIndex;
 
     /// <summary>
     /// Controls the currently selected index of the Page's SortingOrder Picker.
     /// </summary>
     [ObservableProperty]
-    public partial int SelectedSortingOrderIndex {get; set;} = -1;
+    public partial int SelectedSortingOrderIndex {get; set;} = OptionDefaults.SelectedSortingOrderIndex;
 
     /// <summary>
     /// Controls the currently selected index of the Page's SearchingField Picker.
     /// </summary>
     [ObservableProperty]
-    public partial int SelectedSearchingFieldIndex {get; set;} = -1;
+    public partial int SelectedSearchingFieldIndex {get; set;} = OptionDefaults.SelectedSearchingFieldIndex;
 
     /// <summary>
     /// Controls the currently entered Text value of the Page's ListViewSearchBar.
     /// </summary>
     [ObservableProperty]
-    public partial string SearchTerm {get; set;} = "";
+    public partial string SearchTerm {get; set;} = OptionDefaults.SearchTerm;
 
     /// <summary>
     /// Controls the Department that defines the Process currently shown on the Page.
     /// </summary>
     [ObservableProperty]
-    public partial Department? Department {get; set;} = null;
+    public partial Department? Department {get; set;} = OptionDefaults.Department;
 
     /// <summary>
     /// For Selector Pages;
@@ -161,14 +188,14 @@ public partial class DataTablePageOptions() : ObservableObject()
     /// Controls the Process that defines the Parts and Data currently shown on the Page.
     /// </summary>
     [ObservableProperty]
-    public partial Process? Process {get; set;} = null;
+    public partial Process? Process {get; set;} = OptionDefaults.Process;
 
     /// <summary>
     /// For Selector Pages;
     /// Controls the currently selected index of the Page's PageProcess Picker.
     /// </summary>
     [ObservableProperty]
-    public partial int SelectedProcessIndex {get; set;} = -1;
+    public partial int SelectedProcessIndex {get; set;} = OptionDefaults.SelectedProcessIndex;
 
     /// <summary>
     /// Controls the subclass of DataRecord displayed by the Page's ListView.
@@ -180,7 +207,7 @@ public partial class DataTablePageOptions() : ObservableObject()
     /// Controls the boolean condition of whether a Process has been assigned to the Page.
     /// </summary>
     [ObservableProperty]
-    public partial bool IsProcessAssigned {get; set;} = false;
+    public partial bool IsProcessAssigned {get; set;} = OptionDefaults.IsProcessAssigned;
 
     /// <summary>
     /// Updates the Options object to use the Page's Body Header Label as the Body Header.

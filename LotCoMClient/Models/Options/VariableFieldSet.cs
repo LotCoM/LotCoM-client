285bec4a6e1a96761e603c2e1cfd604f81777407
Mason Ritchason <masonritchason@gmail.com>
2025-04-30 13:57:52 -0400

    refactor(VariableFieldSet): convert to ObservableObject; apply ObservableProperty attribute to properties
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMClient/Models/Options/VariableFieldSet.cs
+++ LotCoMClient/Models/Options/VariableFieldSet.cs
@@ -1,10 +1,19 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace LotCoMClient.Models.Options;
 
-public class VariableFieldSet(int? JBKNumber = null, string? LotNumber = null, int? DeburrJBKNumber = null, int? DieNumber = null, string? ModelNumber = null, string? HeatNumber = null) {
-    public int? JBKNumber = JBKNumber;
-    public string? LotNumber = LotNumber;
-    public int? DeburrJBKNumber = DeburrJBKNumber;
-    public int? DieNumber = DieNumber;
-    public string? ModelNumber = ModelNumber;
-    public string? HeatNumber = HeatNumber;
+public partial class VariableFieldSet(int? JBKNumber = null, string? LotNumber = null, int? DeburrJBKNumber = null, int? DieNumber = null, string? ModelNumber = null, string? HeatNumber = null) : ObservableObject ()
+{
+    [ObservableProperty]
+    public partial int? JBKNumber {get; set;} = JBKNumber;
+    [ObservableProperty]
+    public partial string? LotNumber {get; set;} = LotNumber;
+    [ObservableProperty]
+    public partial int? DeburrJBKNumber {get; set;} = DeburrJBKNumber;
+    [ObservableProperty]
+    public partial int? DieNumber {get; set;} = DieNumber;
+    [ObservableProperty]
+    public partial string? ModelNumber {get; set;} = ModelNumber;
+    [ObservableProperty]
+    public partial string? HeatNumber {get; set;} = HeatNumber;
 }
